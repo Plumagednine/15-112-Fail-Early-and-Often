d7fc3e352df9ad020a31519cf45e8eb6a352f79b
Sean Balbale <sean.balbale@gmail.com>
2022-07-21 11:41:18 -0400

    Added title screen

--- Main.py
+++ Main.py
@@ -13,6 +13,18 @@ import math
 ###Helper Functions####################
 #######################################
 
+def almostEqual(d1, d2, epsilon=10**-7): #helper-fn
+    # note: use math.isclose() outside 15-112 with Python version 3.5 or later
+    return (abs(d2 - d1) < epsilon)
+
+import decimal
+def roundHalfUp(d): #helper-fn
+    # Round to nearest with ties going away from zero.
+    rounding = decimal.ROUND_HALF_UP
+    # See other rounding options here:
+    # https://docs.python.org/3/library/decimal.html#rounding-modes
+    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
+
 def create_elipse(canvas, cx, cy, radius, **options):
     canvas.create_oval(cx - radius
                     , cy - radius
@@ -20,8 +32,7 @@ def create_elipse(canvas, cx, cy, radius, **options):
                     , cy + radius
                     , **options)
 
-def getCell(app, x, y, width, height, grid):
-    (gridSize) = grid.getSize()
+def getCell(app, x, y, width, height, gridSize):
     cellWidth = width // gridSize
     cellHeight = height // gridSize
     row =  y//cellHeight
@@ -40,6 +51,11 @@ def getCellBounds(app, row, col, width, height, gridSize):
 
 #######################################
 ###GUI Functions#######################
+#######################################
+
+
+#######################################
+###Game Draw Functions#################
 #######################################
 
 def generateDungeon(app, canvas, grid):
@@ -75,32 +91,69 @@ def drawSidebar(app, canvas):
                        , text=f'Health: {app.player.getHealth()}', fill='#ffffff', font=(app.font,14), anchor = 'w')
     pass
 
+
+#######################################
+###Start Menu Draw Functions###########
+#######################################
+def drawStartMenu(app, canvas):
+    #make interactive grid
+    gridSize = 10
+    startButton = []
+    title = []
+    for row in range(gridSize):
+        for col in range(gridSize):
+            (x0, y0, x1, y1) = getCellBounds(app, row, col, app.width, app.height, gridSize)
+            canvas.create_rectangle(x0, y0, x1, y1, fill='#ffffff', width = 0)
+            if app.startMenuGrid[row][col] == 1: 
+                title.append((row,col))
+            if app.startMenuGrid[row][col] == 2:
+                startButton.append((row,col))
+    
+    #make background image
+    canvas.create_image(app.width//2,0,anchor = 'n', pilImage = app.startMenuBackground)
+    
+    # make title
+    midIndex = len(title)//2
+    tempX0,tempY0,tempX1,tempY1 = getCellBounds(app, title[midIndex][0], title[midIndex][1], app.width, app.height, gridSize)
+    canvas.create_text(tempX0,tempY0, text="15-112: Fail Early and Often", fill='#000000', font=(app.font,60), anchor = 'n',)
+    
+    # make start button
+    midIndex = len(startButton)//2
+    tempX0,tempY0,tempX1,tempY1 = getCellBounds(app, startButton[midIndex][0], startButton[midIndex][1], app.width, app.height, gridSize)
+    canvas.create_text(tempX0,tempY0, text="Start Game", fill='#000000', font=(app.font,60), anchor = 'n')
+    pass
+
+#######################################
+###Redraw All##########################
+#######################################
 def redrawAll(app, canvas): # draw (view) the model in the canvas
     #Draw Background
     canvas.create_rectangle(0, 0, app.width, app.height, fill='#000000')
     
     #start menu
-    if app.startMenu:
+    if app.gameState == 'start':
+        drawStartMenu(app, canvas)
         pass
     
     #win menu
-    if app.winMenu:
+    if app.gameState == 'win':
         canvas.create_text(app.width//2, app.height//2, text="You Win!", fill='#ffffff', font=(app.font,25))
         pass
 
-#######################################
-###While Game is Running###############
-#######################################
     #Draw Game
-    if (not app.startMenu and not app.winMenu):
+    if app.gameState == 'game':
         generateDungeon(app, canvas, app.dungeon)
         drawPlayer(app, canvas, app.player)
         drawSidebar(app, canvas)
     pass      
 
-
 #######################################
 ###Init Functions######################
+#######################################
+
+
+#######################################
+###Game Initializers###################
 #######################################
 def resizeSprite(sprite, width, height):
     sprite = sprite.resize((width, height), Image.NEAREST)
@@ -122,8 +175,6 @@ def initSidebar(app):
     app.sidebarMinHeight = app.gridHeight//app.dungeon.getSize()
     app.sidebarMaxWidth = app.width - app.gridWidth//app.dungeon.getSize()
     app.sidebarMaxHeight = app.height - app.gridHeight//app.dungeon.getSize()
-    print(app.sidebarMinWidth, app.sidebarMinHeight, app.sidebarMaxWidth, app.sidebarMaxHeight)
-    print(app.dungeon.getSize(), app.gridWidth, app.gridHeight)
     pass
 
 def initDimensions(app):
@@ -137,20 +188,54 @@ def updateSpriteDimensions(app):
         app.playerSprite  = resizeSprite(app.playerSprite, app.gridWidth//app.dungeon.getSize(), app.gridHeight//app.dungeon.getSize())
         app.player.updateSprite(app.playerSprite)
 
+#######################################
+###Start Menu Initializers#############
+#######################################
+        
+def initStartMenu(app):
+    # create a 10x10 grid
+    gridSize = 10
+    app.startMenuGrid = []
+    gridRow = []
+    app.startGameButton = [(2,3),(2,4),(2,5),(2,6)]
+    titleCard = [(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)]
+    for row in range(gridSize):
+        for col in range(gridSize):
+            gridRow.append(0)
+        app.startMenuGrid.append(gridRow)
+        gridRow=[]
+        
+    # add buttons
+    for row in range(gridSize):
+        for col in range(gridSize):
+            if (row,col) in titleCard:
+                app.startMenuGrid[row][col] = 1
+            elif (row,col) in app.startGameButton:
+                app.startMenuGrid[row][col] = 2
+                
+    # make image
+    smallerSide = min(app.width, app.height)
+    imgDimensions = int(smallerSide*0.73), smallerSide
+    app.startMenuBackground = app.loadImage('textures\TitleCard.png')
+    app.startMenuBackground = resizeSprite(app.startMenuBackground, imgDimensions[0], imgDimensions[1])
+    pass
+
+#######################################
+###App First Run#######################
+#######################################
 def appStarted(app): # initialize the model (app.xyz)
-#######################################
-###Make System Variables###############
-#######################################
+    #Make System Variables
     app.framerate = 30
     app.timerDelay = 1000//app.framerate
-    app.startMenu = False
-    app.winMenu = False
+    app.gameState = 'start'
     pyglet.font.add_file('font\Vecna-oppx.ttf')
     app.font = 'Vecna-oppx'
     initDimensions(app)
-#######################################
-###Make Dungeon and Player#############
-#######################################
+    
+    #make start Menu
+    initStartMenu(app)
+    
+    #Make Dungeon and Player
     initDungeon(app, 16)
     app.spawnPoint = app.dungeon.getSpawnPoint()
     app.endPoint = app.dungeon.getEndPoint()
@@ -161,14 +246,12 @@ def appStarted(app): # initialize the model (app.xyz)
 def appStopped(app): # cleanup after app is done running
     pass           
 
-
-
 #######################################
 ###User Input##########################
 #######################################
 
 def keyPressed(app, event): # use event.key
-    if (not app.startMenu and not app.winMenu):
+    if app.gameState == 'game':
         grid = app.dungeon.getLayout()
         playerPOS = app.player.getPos()
         #movement
@@ -189,18 +272,17 @@ def keyPressed(app, event): # use event.key
                 app.player.moveRight()
             pass
         if app.player.getPos() == app.endPoint:
-            print("You Win!")
-            app.winMenu = True
+            app.gameState = 'win'
             pass
         #attack
         if event.key == 'e':
             app.player.takeDamage(10)
             pass
         
-    if app.startMenu:
+    if app.gameState == 'start':
         pass
     
-    if app.winMenu:
+    if app.gameState == 'win':
         pass
         
     pass    
@@ -209,6 +291,12 @@ def keyPressed(app, event): # use event.key
     # pass   
 
 def mousePressed(app, event): # use event.x and event.y
+    (row,col) = getCell(app, event.x, event.y, app.width, app.height, 10)
+    if app.gameState == 'start':
+        if (row,col) in app.startGameButton:
+            app.gameState = 'game'
+            pass
+        pass
     pass  
 
 # def mouseReleased(app, event): # use event.x and event.y
@@ -231,6 +319,8 @@ def timerFired(app): # respond to timer events
 def sizeChanged(app): # respond to window size changes
     initDimensions(app)
     updateSpriteDimensions(app)
+    initSidebar(app)
+    initStartMenu(app)
     pass  
 
             
